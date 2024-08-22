88a70edd00f4f66cd7f78f92171c22cafb7844b4
sammycastl <nsmartinezcastl@gmail.com>
2024-08-22 17:10:29 -0600

    butos de agregar funciona

--- (Nelson)paginasinternasebay/controllers/cartservice.js
+++ (Nelson)paginasinternasebay/controllers/cartservice.js
@@ -1,4 +1,9 @@
 function agregarAlCarrito(producto){
     const memoria = localStorage.getItem("cosas")
     console.log(memoria)
+    if(!memoria) {
+        const nuevoProducto = producto;
+        nuevoProducto.cantidad = 1;
+        localStorage.setItem("cosas",JSON.stringify([nuevoProducto]));
+    }
 }
--- (Nelson)paginasinternasebay/controllers/cosas.js
+++ (Nelson)paginasinternasebay/controllers/cosas.js
@@ -2,31 +2,31 @@ const cosas = [
     {
         id: 1,
         name: "cosa 1",  
-        img: "1.png",  
+         
         precio: 111111
     },
     {
         id: 2,
         name: "cosa 2",  
-        img: "2.png",  
+  
         precio: 111111
     },
     {
         id: 3,
         name: "cosa 3",  
-        img: "3.png",  
+  
         precio: 111111
     },
     {
         id: 4,
         name: "cosa 4",  
-        img: "4.png",  
+  
         precio: 111111
     },
     {
         id: 5,
         name: "cosa 5",  
-        img: "5.png",  
+  
         precio: 111111
     }
 ];
