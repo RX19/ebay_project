0e8fafcf36538e95ab3f77439b308f0eeb5ac796
sammycastl <nsmartinezcastl@gmail.com>
2024-08-23 21:17:40 -0600

    servicio al cliente

---
+++ (Nelson)paginasinternasebay/controllers/servicioClienteController.js
@@ -0,0 +1,10 @@
+function actualizarCantidadCarrito() {
+    let cantidadCarrito = localStorage.getItem('carritoCantidad') || 0;
+    console.log(localStorage.getItem('carritoCantidad'));
+    console.log("Cantidad en el carrito:", cantidadCarrito); 
+    document.getElementById('cuenta-carrito').textContent = cantidadCarrito;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    actualizarCantidadCarrito();
+});
